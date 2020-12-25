071d4dc02094ef5c8dbbbe3eb880686c3b72511d
David Giraldo <davidgiraldobueno@gmail.com>
2020-12-25 13:49:54 -0500

    local storage

--- src/context/context.js
+++ src/context/context.js
@@ -2,7 +2,7 @@ import React, { useReducer, createContext } from "react";
 
 import contextReducer from "./contextReducer.js";
 
-const initialState = [];
+const initialState = JSON.parse(localStorage.getItem('transactions')) || [{"amount":10,"category":"Investments","type":"Income","date":"2020-12-25","id":"d45bace1-e10e-4a32-9cec-73a49ea8aed4"},{"amount":50,"category":"Internet","type":"Expense","date":"2020-12-25","id":"e8b4779a-7493-41b6-adad-19974a587487"},{"amount":100,"category":"Food","type":"Expense","date":"2020-12-25","id":"a60d1222-e7ab-45fb-b8da-e79cc993794b"},{"amount":150,"category":"Gifts","type":"Income","date":"2020-12-25","id":"1824b004-45ee-4ece-a021-450fe09c34ff"},{"amount":250,"category":"Salary","type":"Income","date":"2020-12-25","id":"8039e865-f7fd-4ac3-aab1-9d6b3da6c9b3"}];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
