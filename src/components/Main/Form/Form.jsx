927d1e9c1d7adda8ed1c003b4dc37ac32c64ac51
David Giraldo <davidgiraldobueno@gmail.com>
2020-12-25 22:06:42 -0500

    snackbar

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -20,6 +20,7 @@ import {
   incomeCategories,
   expenseCategories,
 } from "../../../constants/categories";
+import CustomizedSnackbar from '../../Snackbar/Snackbar';
 
 const initialState = {
   amount: "",
@@ -33,15 +34,18 @@ const Form = () => {
   const [formData, setformData] = useState(initialState);
   const { addTransaction } = useContext(ExpenseTrackerContext);
   const { segment } = useSpeechContext();
+  const [open, setOpen] = React.useState(false);
 
   const createTransaction = () => {
     if (Number.isNaN(Number(formData.amount)) || !formData.date.includes('-')) return;
-    
+
     const transaction = {
       ...formData,
       amount: Number(formData.amount),
       id: uuidv4(),
     };
+
+    setOpen(true);
     addTransaction(transaction);
     setformData(initialState);
   };
@@ -110,6 +114,7 @@ const Form = () => {
 
   return (
     <Grid container spacing={2}>
+      <CustomizedSnackbar open={open} setOpen={setOpen} />
       <Grid item xs={12}>
         <Typography align="center" variant="subtitle2" gutterBottom>
           {segment ? (
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,33 +1,49 @@
-import React from 'react';
-import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import React, { useContext } from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+import { ExpenseTrackerContext } from "../../context/context";
 
-import useStyles from './styles';
-import Form from './Form/Form';
-import List from './List/List';
+import useStyles from "./styles";
+import Form from "./Form/Form";
+import List from "./List/List";
 
 const Main = () => {
-    const classes = useStyles();
+  const classes = useStyles();
+  const { balance } = useContext(ExpenseTrackerContext);
 
-    return (
-        <Card >
-            <CardHeader title='Expense Tracker' subheader='Powered by Speechly' />
-            <CardContent>
-                <Typography align='center' variant='h5'>Total Balance $100</Typography>
-                <Typography variant='subtitle1' style={{ lineHeight: '1.5em', marginTop: '20px'}} > {/* Info Card */}
-                Try saying: Add income for $100 in category salary
-                </Typography>
-                <Divider />
-                <Form />
-            </CardContent>
-            <CardContent className={classes.cartContent}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <List />
-                    </Grid>
-                </Grid>
-            </CardContent>
-        </Card>
-    )
-}
+  return (
+    <Card>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance {balance > 0 ? `$${balance}` : `-$${Math.abs(balance)}`}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          {" "}
+          {/* Info Card */}
+          Try saying: Add income for $100 in category salary
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default Main;
